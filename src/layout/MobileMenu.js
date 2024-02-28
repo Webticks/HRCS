96b6fc5e5c5ec2f36b60d073bc52d94bdfa7c7ee
fuzail08 <muhammadfuzail33@gmail.com>
2024-02-28 06:27:42 +0500

    about us page added

--- src/layout/MobileMenu.js
+++ src/layout/MobileMenu.js
@@ -76,7 +76,8 @@ const MobileMenu = () => {
               <i className="far fa-angle-down" />
             </span>
           </li>
-          <li className="menu-item has-children">
+          
+          {/* <li className="menu-item has-children">
             <a href="#">Team</a>
             <ul className="sub-menu" style={activeLi("team")}>
               <li>
@@ -206,7 +207,7 @@ const MobileMenu = () => {
             <span className="dd-trigger" onClick={() => activeMenuSet("blog")}>
               <i className="far fa-angle-down" />
             </span>
-          </li>
+          </li> */}
           <li className="menu-item">
             <Link legacyBehavior href="contact">
               Contact
