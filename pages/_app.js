98ad008629ed16e9d5077fdee288b3e4a1c3d206
fuzail08 <muhammadfuzail33@gmail.com>
2024-03-23 03:56:58 +0500

    phils analytics key added

--- pages/_app.js
+++ pages/_app.js
@@ -14,14 +14,14 @@ function MyApp({ Component, pageProps }) {
   return (
     <Fragment>
       <Head>
-      <script
+        <script
           dangerouslySetInnerHTML={{
             __html: `
               (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
               new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
               j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
               'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-              })(window,document,'script','dataLayer','G-FJ5JP1QY0R');
+              })(window,document,'script','dataLayer','G-Y6X7TJGY23');
             `,
           }}
         />
