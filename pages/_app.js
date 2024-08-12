a089450fc1c83734d78dac8ce09ff35012a2cc92
fuzail08 <muhammadfuzail33@gmail.com>
2024-08-12 11:01:38 +0500

    hehehe

--- pages/_app.js
+++ pages/_app.js
@@ -1,79 +1,79 @@
-// import Head from "next/head";
-// import { Fragment, useEffect, useState } from "react";
-// import "slick-carousel/slick/slick-theme.css";
-// import PreLoader from "../src/layout/PreLoader";
-// import "../styles/globals.css";
-// function MyApp({ Component, pageProps }) {
-//   const [loader, setLoader] = useState(true);
-//   useEffect(() => {
-//     setTimeout(() => {
-//       setLoader(false);
-//     }, 1000);
-//   }, []);
+import Head from "next/head";
+import { Fragment, useEffect, useState } from "react";
+import "slick-carousel/slick/slick-theme.css";
+import PreLoader from "../src/layout/PreLoader";
+import "../styles/globals.css";
+function MyApp({ Component, pageProps }) {
+  const [loader, setLoader] = useState(true);
+  useEffect(() => {
+    setTimeout(() => {
+      setLoader(false);
+    }, 1000);
+  }, []);
 
-//   return (
-//     <Fragment>
-//       <Head>
-//         <script
-//           dangerouslySetInnerHTML={{
-//             __html: `
-//               (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-//               new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-//               j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-//               'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-//               })(window,document,'script','dataLayer','G-Y6X7TJGY23');
-//             `,
-//           }}
-//         />
-//         <title>
-//           HRCS
-//         </title>
-//         {/*====== Favicon Icon ======*/}
-//         <link
-//           rel="shortcut icon"
-//           href="assets/images/hrcsfavicon.ico"
-//           type="image/png"
-//         />
-//         {/*====== FontAwesome css ======*/}
-//         <link
-//           rel="stylesheet"
-//           href="assets/fonts/fontawesome/css/all.min.css"
-//         />
-//         {/*====== Flaticon css ======*/}
-//         <link rel="stylesheet" href="assets/fonts/flaticon/flaticon.css" />
-//         {/*====== Bootstrap css ======*/}
-//         <link
-//           rel="stylesheet"
-//           href="assets/vendor/bootstrap/css/bootstrap.min.css"
-//         />
-//         {/*====== magnific-popup css ======*/}
-//         <link
-//           rel="stylesheet"
-//           href="assets/vendor/magnific-popup/dist/magnific-popup.css"
-//         />
-//         {/*====== Slick-popup css ======*/}
-//         <link rel="stylesheet" href="assets/vendor/slick/slick.css" />
-//         {/*====== Jquery UI css ======*/}
-//         <link
-//           rel="stylesheet"
-//           href="assets/vendor/jquery-ui/jquery-ui.min.css"
-//         />
-//         {/*====== Nice Select css ======*/}
-//         <link
-//           rel="stylesheet"
-//           href="assets/vendor/nice-select/css/nice-select.css"
-//         />
-//         {/*====== Animate css ======*/}
-//         <link rel="stylesheet" href="assets/vendor/animate.css" />
-//         {/*====== Default css ======*/}
-//         <link rel="stylesheet" href="assets/css/default.css" />
-//         {/*====== Style css ======*/}
-//         <link rel="stylesheet" href="assets/css/style.css" />
-//       </Head>
-//       {loader && <PreLoader />}
-//       <Component {...pageProps} />
-//     </Fragment >
-//   );
-// }
+  return (
+    <Fragment>
+      <Head>
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+              new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+              j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+              'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+              })(window,document,'script','dataLayer','G-Y6X7TJGY23');
+            `,
+          }}
+        />
+        <title>
+          HRCS
+        </title>
+        {/*====== Favicon Icon ======*/}
+        <link
+          rel="shortcut icon"
+          href="assets/images/hrcsfavicon.ico"
+          type="image/png"
+        />
+        {/*====== FontAwesome css ======*/}
+        <link
+          rel="stylesheet"
+          href="assets/fonts/fontawesome/css/all.min.css"
+        />
+        {/*====== Flaticon css ======*/}
+        <link rel="stylesheet" href="assets/fonts/flaticon/flaticon.css" />
+        {/*====== Bootstrap css ======*/}
+        <link
+          rel="stylesheet"
+          href="assets/vendor/bootstrap/css/bootstrap.min.css"
+        />
+        {/*====== magnific-popup css ======*/}
+        <link
+          rel="stylesheet"
+          href="assets/vendor/magnific-popup/dist/magnific-popup.css"
+        />
+        {/*====== Slick-popup css ======*/}
+        <link rel="stylesheet" href="assets/vendor/slick/slick.css" />
+        {/*====== Jquery UI css ======*/}
+        <link
+          rel="stylesheet"
+          href="assets/vendor/jquery-ui/jquery-ui.min.css"
+        />
+        {/*====== Nice Select css ======*/}
+        <link
+          rel="stylesheet"
+          href="assets/vendor/nice-select/css/nice-select.css"
+        />
+        {/*====== Animate css ======*/}
+        <link rel="stylesheet" href="assets/vendor/animate.css" />
+        {/*====== Default css ======*/}
+        <link rel="stylesheet" href="assets/css/default.css" />
+        {/*====== Style css ======*/}
+        <link rel="stylesheet" href="assets/css/style.css" />
+      </Head>
+      {loader && <PreLoader />}
+      <Component {...pageProps} />
+    </Fragment >
+  );
+}
 
-// export default MyApp;
+export default MyApp;
