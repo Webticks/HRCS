c232ce9ddcaf957b1cdaa1238ae79f2301eec594
mfuzail33 <muhammadfuzail33@gmail.com>
2024-10-26 02:29:26 +0500

    analytics

--- pages/_app.js
+++ pages/_app.js
@@ -15,13 +15,16 @@ function MyApp({ Component, pageProps }) {
     <Fragment>
       <Head>
         <script
+          async
+          src="https://www.googletagmanager.com/gtag/js?id=G-Y6X7TJGY23"
+        ></script>
+        <script
           dangerouslySetInnerHTML={{
             __html: `
-              (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-              new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-              j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-              'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-              })(window,document,'script','dataLayer','G-Y6X7TJGY23');
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', 'G-Y6X7TJGY23');
             `,
           }}
         />
